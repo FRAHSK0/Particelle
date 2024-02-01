accffb1a961f32790120230625aade17b4835ec5
FRAHSK0 <126199177+FRAHSK0@users.noreply.github.com>
2024-02-01 15:26:46 +0100

    Aggiungere i file di progetto.

---
+++ Particella.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Particelle
+{
+    internal class Particella
+    {
+        int posizione;
+        public Particella(int posizione)
+        {
+            this.posizione = posizione;
+
+
+
+        }
+        public int Posizione
+        { 
+        get=>posizione; 
+        set=>this.posizione = value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Particelle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = 100;
+            List<Particella> list = new List<Particella>();
+            Random P = new Random();
+            using (StreamWriter sw = new StreamWriter("c:\\Users\\Francesco\\Desktop\\P.csv"))
+            {
+                for (int i = 0; i < n; i++)
+            {  
+               Particella p=new Particella(0);   
+                list.Add(p);
+            }
+            for (int i = 0; i < 31; i++)
+            {
+                foreach (Particella p in list)
+                {
+                    if (P.Next(1, 6) == 1)
+                    {
+                        if (p.Posizione == 0)   
+                        p.Posizione = 1;
+                        else 
+                        p.Posizione = 0;
+                    }
+                }
+                int c;
+                c = 0;
+
+
+                foreach (Particella p in list)
+                {
+                    if (p.Posizione==1)
+                    {
+                        c++;
+                    }
+                }
+                Console.WriteLine("ci sono al tempo {0} in pozione B {1} particelle e in pozione A {2} particelle   ", i, c,n-c);
+                sw.WriteLine($"{i};tempo;numero particelle in B;{c};numero di particelle in A {n-c};");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
